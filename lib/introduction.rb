9db63a2dc884a31583cfff5ebd48f569ed6ea17e
Daniel Costelloe <danny.costelloe@gmail.com>
2021-02-15 23:21:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,9 +2,6 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
 
 def introduction(name, language)
   puts "Hi, my name is #{name} and i'm learning to program in #{language}."
